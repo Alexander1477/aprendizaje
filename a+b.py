673fa19f316ae11a4bb8b942476838b024b7b0aa
Alexander <ftoledomendez0@gmail.com>
2026-07-08 13:38:58 -0600

    Add files via upload

---
+++ a+b.py
@@ -0,0 +1,13 @@
+a = int(input("introduce un valor: "))
+b=int(input("introduce otro valor: "))
+
+c=a+b
+
+if a == 6 and b == 7:
+    print("SIX SEBEEEENNNNN")
+elif (a+b) == 13:
+    print("Mientras más me lo mamas más me crece")
+elif (a+b) == 14:
+    print("Prende cam")    
+else:
+    print(f"tu resultado es: {c}")
